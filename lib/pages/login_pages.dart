05ffc2c1d1d6eba1ed243aed6f8354771d746b2a
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-24 13:57:57 +0500

    MediaQuery .sizeOf

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -11,19 +11,46 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("App Name",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700)),
-          TextField(
-            decoration: InputDecoration(
-                hintText: "login",
-                filled: true,
-                fillColor: Colors.white.withOpacity(.5),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12)))),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                Spacer(),
+                Text("App Name",
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.w700)),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Login",
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(.5),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)))),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Parol",
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(.5),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)))),
+                ),
+                SizedBox(height: 16),
+                SizedBox(
+                    width: double.maxFinite,
+                    height: 60,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: Text("Kirish"))),
+                Spacer(),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
